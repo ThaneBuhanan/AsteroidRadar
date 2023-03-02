e7d4afd8b5ebab49c1fd813d2cd9bbbf70f80b25
ThaneBuhanan <thanebuhanan@gmail.com>
2023-03-01 21:41:25 -0700

    asteroid list item and asteroid adapter added.

---
+++ app/src/main/java/com/udacity/asteroidradar/main/AsteroidAdapter.kt
@@ -0,0 +1,55 @@
+package com.udacity.asteroidradar.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.databinding.AsteroidListItemBinding
+
+class AsteroidAdapter(val onClick: (Asteroid) -> Unit) :
+    ListAdapter<Asteroid, ViewHolder>(DiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val asteroid = getItem(position)
+
+        holder.itemView.setOnClickListener {
+            onClick(asteroid)
+        }
+        holder.bind(asteroid)
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<Asteroid>() {
+        override fun areItemsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+}
+
+class ViewHolder private constructor(private val binding: AsteroidListItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(asteroid: Asteroid) {
+        binding.asteroid = asteroid
+
+        binding.executePendingBindings()
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): ViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = AsteroidListItemBinding.inflate(layoutInflater, parent, false)
+
+            return ViewHolder(binding)
+        }
+    }
+}
